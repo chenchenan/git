8787127c24ea430d1d4a38465072906797303611
86158 <2534427174@qq.com>
2021-06-29 23:07:59 +0800

    git 05

--- src/main/java/com/liyanfeng/GitTest.java
+++ src/main/java/com/liyanfeng/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git03");
         System.out.println("hello git04");
         System.out.println("hello git05");
+        System.out.println("hello git06");
     }
 }
